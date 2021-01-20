5f8380516dc8fc8f0c203dd8b931cd8536850e58
Am0rphous <29645801+Am0rphous@users.noreply.github.com>
2021-01-20 20:23:02 +0100

    Create Honeypots.sh

---
+++ Hacking/Honeypots.sh
@@ -0,0 +1,18 @@
+# Honeypots
+````
+https://github.com/mushorg
+````
+-  Mushorg's Collection
+  - [Glastopf](https://github.com/mushorg/glastopf) - _Web Application Honeypot [glastopf.org](http://glastopf.org)._
+  - [Snare](https://github.com/mushorg/snare) - _Super Next generation Advanced Reactive honEypot [ushmush.org](http://mushmush.org)._
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
