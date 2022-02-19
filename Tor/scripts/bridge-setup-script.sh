3e21f889695655c4df6ae57a65ed8d226d98a01d
Am0rphous <29645801+Am0rphous@users.noreply.github.com>
2022-02-19 14:16:42 +0100

    Update bridge-setup-script.sh

--- Tor/scripts/bridge-setup-script.sh
+++ Tor/scripts/bridge-setup-script.sh
@@ -5,12 +5,24 @@
 sudo apt update && sudo apt install
 
 sudo cat << EOF > /etc/tor/torrc
-//date: 01.01.22
+//date: xx.xx.xx
 
-// RelayBandwidthRate 125 KB
-// MaxAdvertisedBandwidth 125 KB
-// MaxMemInQueues 1024 MB
-// ORPort 443 IPv4Only
+BridgeRelay 1
+RunAsDaemon 1
+ControlPort 9051
+CookieAuthentication 1
+ORPort 443
+ServerTransportPlugin obfs4 exec /usr/bin/obfs4proxy
+ServerTransportListenAddr obfs4 YOURIPADDRESSHERE:80
+ExtORPort auto
+Nickname Valsec
+ContactInfo Anonymous
+DataDirectory /var/lib/tor
+//MaxMemInQueues 512 MB         ##Limit RAM usage
+RelayBandwidthRate 1024 KB      ##1 Mbit
+MaxAdvertisedBandwidth 1024 KB  ##1 Mbit
+
+// ORPort 443 IPv4Only    ##Need to check syntax here
 //done
 EOF
 
@@ -18,4 +30,5 @@ EOF
 sudo netstat -tulpn | grep tor
 
 printf "\nI am done..\n"
-sudo service tor@default status
+sudo service tor@default restart
+sudo service tor@default status 
