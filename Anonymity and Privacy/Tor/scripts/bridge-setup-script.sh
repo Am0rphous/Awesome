03c1ee8153d902f44aa18a50a5d95ce689ca8944
Am0rphous <29645801+Am0rphous@users.noreply.github.com>
2026-01-11 17:10:08 +0100

    Update bridge-setup-script.sh

--- Anonymity and Privacy/Tor/scripts/bridge-setup-script.sh
+++ Anonymity and Privacy/Tor/scripts/bridge-setup-script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-#work in progress
+# work in progress
+# using port 3333 and 4444 - change these if you wish, but these needs to be externally reachable
+# https://community.torproject.org/relay/setup/bridge/debian-ubuntu/
 
 sudo apt update && sudo apt install tor obfs4proxy
 
@@ -11,18 +13,17 @@ BridgeRelay 1
 RunAsDaemon 1
 ControlPort 9051
 CookieAuthentication 1
-ORPort 443 IPv4Only                                   ##Use only ipV4
+ORPort 3333 IPv4Only                                    # CHANGE PORT 1
 ServerTransportPlugin obfs4 exec /usr/bin/obfs4proxy
-ServerTransportListenAddr obfs4 YOURIPADDRESSHERE:80
+ServerTransportListenAddr obfs4 0.0.0.0:4444            # CHANGE PORT 2
 ExtORPort auto
 Nickname Anonymous
 ContactInfo Anonymous
 DataDirectory /var/lib/tor
-//MaxMemInQueues 4096 MB                            ##Limit RAM usage
-RelayBandwidthRate 10 MB                            ##10 Mbit
-MaxAdvertisedBandwidth 10 MB                        ##10 Mbit
+//MaxMemInQueues 4096 MB                            ##Limit RAM usage. No need to
+RelayBandwidthRate 10 MB                            ##10 Mbit. No need
+MaxAdvertisedBandwidth 10 MB                        ##10 Mbit. No need
 
-// ORPort 443 IPv4Only                              ##Need to check syntax here
 //done
 EOF
 
@@ -31,5 +32,4 @@ sudo netstat -tulpn | grep tor
 
 printf "\nI am done..\n"
 systemctl enable --now tor.service
-sudo service tor@default restart
-sudo service tor@default status 
+sudo service tor@default restart && sudo service tor@default status 
